aa9e991e349f29ace81746eae37239b0d2a515b4
B_jin <65752350+rlaqudwls25@users.noreply.github.com>
2024-03-25 19:20:59 +0900

    [feat]: 프로젝트 초기 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
+import { Global } from '@emotion/react'
+import globalStyles from '@/styles/globalStyles'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient({})
+
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
+  )
 }
