a20cd241d8b4b5cacf366944007de8ef6e23a113
B_jin <65752350+rlaqudwls25@users.noreply.github.com>
2024-03-29 14:40:38 +0900

    [fix]: query parameter 수정

--- src/apis/getUser.ts
+++ src/apis/getUser.ts
@@ -7,10 +7,13 @@ export const getUser = async ({
   userName: string
   pageParam: number
 }) => {
-  const res = await customAxios(
-    Method.GET,
-    `search/users?q=${userName}&per_page=20&page=${pageParam}`,
-  )
+  const res = await customAxios(Method.GET, 'search/users', {
+    params: {
+      q: userName,
+      per_page: 20,
+      page: pageParam,
+    },
+  })
 
   return res
 }
